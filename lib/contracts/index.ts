a9655632947c0bff6526a999df51291d475c8f65
Lucian Buzzo <lucian.buzzo@gmail.com>
2022-06-09 16:54:24 +0100

    Improve performance of mirror triggered actions
    This change splits the existing triggered action into two, with much
    more specific filters that prevent the integration running unnecessarily
    on non-discourse threads.
    
    Change-type: patch
    Signed-off-by: Lucian Buzzo <lucian.buzzo@gmail.com>

--- lib/contracts/index.ts
+++ lib/contracts/index.ts
@@ -1,10 +1,12 @@
 import type { ContractDefinition } from '@balena/jellyfish-types/build/core';
 import { channelDiscussionThreads } from './channel-discussion-threads';
 import { triggeredActionIntegrationDiscourseMirrorEvent } from './triggered-action-integration-discourse-mirror-event';
+import { triggeredActionIntegrationDiscourseMirrorThread } from './triggered-action-integration-discourse-mirror-thread';
 import { viewAllForumThreads } from './view-all-forum-threads';
 
 export const contracts: ContractDefinition[] = [
 	channelDiscussionThreads,
 	triggeredActionIntegrationDiscourseMirrorEvent,
+	triggeredActionIntegrationDiscourseMirrorThread,
 	viewAllForumThreads,
 ];
