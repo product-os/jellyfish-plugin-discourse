a3824c666fab5c63cdbcb3354bb44bac35e6f6e9
Josh Bowling <josh@balena.io>
2022-09-13 15:58:43 +0900

    CI: Move to Flowzone
    Change-type: patch
    Signed-off-by: Josh Bowling <josh@monarci.com>

--- scripts/run-ci-tasks.sh
+++ scripts/run-ci-tasks.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# Set necessary environment variables
-export INTEGRATION_DISCOURSE_TOKEN=$(cat /run/secrets/integration_discourse_token)
-export INTEGRATION_DISCOURSE_USERNAME=$(cat /run/secrets/integration_discourse_username)
-
 # Run tasks
 scripts/delete-test-data.js || true
 npm run test:integration
