1a08d90ea5db748965bc2c40d3fcc802abe44713
Josh Bowling <josh@balena.io>
2023-08-12 17:12:09 +0900

    Bump internal dependencies
    Change-type: patch

--- scripts/test-integration.sh
+++ scripts/test-integration.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-npx jest --runInBand --forceExit test/integration/mirror.spec.ts && \
-	npx jest --forceExit test/integration/index.spec.ts && \
+npx jest --forceExit test/integration/index.spec.ts && \
 	npx jest --runInBand --forceExit test/integration/translate.spec.ts
